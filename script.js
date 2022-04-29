f07ae2d9b32716e082d7b6a438434726d6b1480b
Leonardo Zanette <leonardobzanette@gmail.com>
2022-04-29 17:15:14 -0300

    Requisito 7 - DONE

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-function adiciona()  {
+function adiciona() {
     let input = document.getElementById('texto-tarefa');
     let button = document.getElementById('criar-tarefa');
     let lista = document.getElementById('lista-tarefas')
-   
-    button.addEventListener('click', function(){
+
+    button.addEventListener('click', function () {
         let li = document.createElement('li');
         li.innerText = input.value
+        li.className = 'lista'
         lista.appendChild(li)
-        input.value=""
-        
+        input.value = ""
+
     })
 }
 adiciona()
+
+// function alteraBackground() {
+//     let li = document.querySelector('.lista');
+//     li.addEventListener('click', function(event) {
+//         event.target.style.backgroundColor = 'gray'
+//     })
+// }
+// alteraBackground()
+
+function pintaDeCinza() {
+    document.addEventListener('click', function (event) {
+        if (event.target.className == 'lista') {
+            event.target.style.backgroundColor = 'gray'
+        }
+    })
+}
+
+pintaDeCinza()
