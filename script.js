752373f4cd153464fcb230f29a72e1bbe6bf9740
Leonardo Zanette <leonardobzanette@gmail.com>
2022-04-30 12:05:21 -0300

    Requisito 9 - DONE

--- script.js
+++ script.js
@@ -25,21 +25,41 @@ adiciona()
 function pintaDeCinza() {
     document.addEventListener('click', function (event) {
         let selected = document.querySelector('.selected');
+
         if (event.target.className == 'lista') {
+
             if (selected == null) {
                 event.target.classList.add('selected')
             } else {
                 selected.classList.remove('selected');
                 event.target.classList.add('selected')
             }
-
         }
+    })
+}
+pintaDeCinza()
 
+function doubleClick() {
+
+    document.addEventListener('dblclick', function (event) {
+       if(event.target.classList.contains('lista')) {
+
+           if (event.target.classList.contains('completed')) {
+               event.target.classList.remove('completed')
+            } else {
+                
+                event.target.classList.add('completed');
+            }
+            // if(event.target.classList.indexOf('completed')< 0) {
+                // event.target.classList.add('completed') 
+                // }       
+            }
     }
     )
 }
+doubleClick()
 
-pintaDeCinza()
+
 
 // function changeSelected(event) {
 //     let selected = document.querySelector('.selected');
